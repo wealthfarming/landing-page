9e4736ee394f6e0c94cccb77ef98a7a3682f7a35
PaTuanas <phanhtuan05@gmail.com>
2025-04-16 11:16:23 +0700

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -15,19 +15,19 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen  font-[family-name:var(--font-geist-sans)]">
 
-      <HeaderDesktopFull changeAt={300} />
+      {/* <HeaderDesktopFull changeAt={300} /> */}
       <main className="flex flex-col body row-start-2 justify-center items-center sm:items-start w-full relative">
-        <Image src="/images/img/product_base.jpg" alt="Product Banner" width={735} height={80} className="absolute w-full -z-10 top-[-400px] object-cover hidden lg:block" />
-        <HomeSection1 />
+        {/* <Image src="/images/img/product_base.jpg" alt="Product Banner" width={735} height={80} className="absolute w-full -z-10 top-[-400px] object-cover hidden lg:block" /> */}
+        {/* <HomeSection1 />
         <HomeSection2 />
         <HomeSection3 />
         <HomeSection4 />
-        <HomeSection5 />
+        <HomeSection5 /> */}
         <Section5 />
         <Section6 />
-        <HomeSection6 />
-        <HomeSection7 />
-        <FooterFull active={'product'} />
+        {/* <HomeSection6 />
+        <HomeSection7 /> */}
+        {/* <FooterFull active={'product'} /> */}
       </main>
     </div>
   );
