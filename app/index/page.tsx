f90904fdcf3ade5ed00f355196dc3bf484b6b540
HDNghia <huynhdangnghia68@gmail.com>
2025-04-18 08:09:44 +0700

    test

--- app/index/page.tsx
+++ app/index/page.tsx
@@ -1,32 +1,26 @@
 "use client";
 
 import React, { Suspense } from "react";
-import dynamic from 'next/dynamic';
-
-const Header = dynamic(() => import("@/components/header/header-desktop"), {
-  ssr: false,
-  loading: () => <div className="h-[72px]" />
-});
 
 import IntroSection from "@/components/index/intro-section";
-import ProblemSection from "@/components/index/problem-section";
-import SecuritySection from "@/components/index/security-section";
-import SolutionSection from "@/components/index/solution-section";
-import NFTFutureSection from "@/components/index/nft-futures-ection";
-import SecurityDetailsSection from "@/components/index/security-details-section";
-import ProcessSection from "@/components/index/process-section";
-import PartnersSection from "@/components/index/partners-section";
-import ContactSection from "@/components/index/contact-section";
-import FAQSection from "@/components/index/faq-section";
-import Footer from "@/components/index/footer";
+// import ProblemSection from "@/components/index/problem-section";
+// import SecuritySection from "@/components/index/security-section";
+// import SolutionSection from "@/components/index/solution-section";
+// import NFTFutureSection from "@/components/index/nft-futures-ection";
+// import SecurityDetailsSection from "@/components/index/security-details-section";
+// import ProcessSection from "@/components/index/process-section";
+// import PartnersSection from "@/components/index/partners-section";
+// import ContactSection from "@/components/index/contact-section";
+// import FAQSection from "@/components/index/faq-section";
+// import Footer from "@/components/index/footer";
 
 const IndexLandingPage = () => {
   return (
     <div className="min-h-screen">
       <div className="overflow-hidden w-full bg-white max-md:max-w-full">
-        <Suspense fallback={<div className="h-[72px]" />}>
+        {/* <Suspense fallback={<div className="h-[72px]" />}>
           <Header changeAt={0} />
-        </Suspense>
+        </Suspense> */}
         <IntroSection />
         {/* <ProblemSection />
         <SecuritySection />
