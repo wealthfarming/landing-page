c0ae18e5caf6b4c85521a8fb734654d670762b4b
HDNghia <huynhdangnghia68@gmail.com>
2025-04-24 16:56:28 +0700

    update layout

--- components/header/header-desktop.tsx
+++ components/header/header-desktop.tsx
@@ -94,11 +94,11 @@ export default function HeaderDesktopFull({ changeAt }: { changeAt: number }) {
                     {/* Language Select */}
                     {!isScrolled && isDesktop && (
                         <Select  onValueChange={handleSelectChange} defaultValue={currentLanguage}>
-                            <SelectTrigger className="!h-[43px] flex flex-row items-center justify-between rounded-none bg-background !rounded-[4px] w-[80px]">
+                            <SelectTrigger className="!h-[43px] flex flex-row items-center justify-between rounded-none bg-background !rounded-[4px] w-[100px]">
                                 <GlobeSimple size={18} className="min-w-[18px] min-h-[18px]" />
                                 <SelectValue placeholder={changeLanguage} />
                             </SelectTrigger>
-                            <SelectContent className="rounded-none z-[1001] !w-[80px] !max-w-[80px] !min-w-[80px]">
+                            <SelectContent className="rounded-none z-[1001] !w-[100px] !max-w-[100px] !min-w-[100px]">
                                 <SelectItem  value="en">{t('currentLanguage_en')}</SelectItem>
                                 <SelectItem  value="vi">{t('currentLanguage_vi')}</SelectItem>
                             </SelectContent>
