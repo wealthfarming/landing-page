44123130a49e4241cfc48339ec03ee1447badec3
HDNghia <huynhdangnghia68@gmail.com>
2025-04-25 14:53:30 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
+// next.config.js
 module.exports = {
-    reactStrictMode: true,
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.jsx/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react']
-          }
+  reactStrictMode: true,
+  output: 'export',
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.jsx/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react']
         }
-      });
-      return config;
-    },
-  };
-  
+      }
+    });
+    return config;
+  },
+};
