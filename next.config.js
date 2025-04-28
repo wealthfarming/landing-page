b5d958006ce399e21b4955612525f461e7146918
HDNghia <huynhdangnghia68@gmail.com>
2025-04-28 20:39:14 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,19 +1,14 @@
-// next.config.js
-
 module.exports = {
   reactStrictMode: true,
   output: 'export',
-  basePath: '/landing-page',
-  assetPrefix: '/landing-page/',
+  basePath: '',
+  assetPrefix: '',
   trailingSlash: true,
-
-  // ← disable the optimizer so <Image> falls back to plain <img src="…">
   images: {
     unoptimized: true,
   },
 
   webpack(config) {
-    // …your existing babel-loader tweak…
     return config
   },
 }
