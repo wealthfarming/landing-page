ce63841cf7bbe2f6368943720f817d42d1d5cb68
HDNghia <huynhdangnghia68@gmail.com>
2025-05-14 11:53:59 +0700

    update

--- components/index/contact-section.tsx
+++ components/index/contact-section.tsx
@@ -33,63 +33,8 @@ const ContactSection: React.FC = () => {
                 />
                 <p className="!text-[var(--primary)]">contact@wealthfarming.org</p>
               </div>
-
-              {/* Addresses */}
-              <div className="flex gap-2 items-start w-full max-md:max-w-full">
-                <img
-                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/9146726f4a56bf97cdef6fcce5f7d515336b0ffc?placeholderIfAbsent=true&apiKey=29da101503f047abb81734f632fb9540"
-                  alt="Location icon"
-                  className="object-contain shrink-0 w-6 aspect-square"
-                />
-                <div className="flex-1 shrink basis-0">
-                  <p className="font-semibold mb-1">USA:</p>
-                  <p>BeQ Holdings INC (US),</p>
-                  <p className="">200 Continental Drive, Newark, DE 19713, USA</p>
-                </div>
-              </div>
-
-              <div className="flex gap-2 items-start w-full max-md:max-w-full">
-                <img
-                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/9146726f4a56bf97cdef6fcce5f7d515336b0ffc?placeholderIfAbsent=true&apiKey=29da101503f047abb81734f632fb9540"
-                  alt="Location icon"
-                  className="object-contain shrink-0 w-6 aspect-square"
-                />
-                <div className="flex-1 shrink basis-0">
-                  <p className="font-semibold mb-1">Singapore:</p>
-                  <p>14 Robinson Road #08-01A,</p>
-                  <p>Far East Finance Building,</p>
-                  <p>Singapore (048545)</p>
-                </div>
-              </div>
-
-              <div className="flex gap-2 items-start w-full max-md:max-w-full">
-                <img
-                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/9146726f4a56bf97cdef6fcce5f7d515336b0ffc?placeholderIfAbsent=true&apiKey=29da101503f047abb81734f632fb9540"
-                  alt="Location icon"
-                  className="object-contain shrink-0 w-6 aspect-square"
-                />
-                <div className="flex-1 shrink basis-0">
-                  <p className="font-semibold mb-1">Vietnam:</p>
-                  <p>67 Dinh Thi Thi Street, Van Phuc City,</p>
-                  <p>Thu Duc District, Ho Chi Minh City</p>
-                </div>
-              </div>
-
-              <div className="flex gap-2 items-start w-full max-md:max-w-full">
-                <img
-                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/9146726f4a56bf97cdef6fcce5f7d515336b0ffc?placeholderIfAbsent=true&apiKey=29da101503f047abb81734f632fb9540"
-                  alt="Location icon"
-                  className="object-contain shrink-0 w-6 aspect-square"
-                />
-                <div className="flex-1 shrink basis-0">
-                  <p className="font-semibold mb-1">UK:</p>
-                  <p>128 City Road, London,</p>
-                  <p>United Kingdom, EC1V 2NX</p>
-                </div>
-              </div>
-
               {/* Social Media Links */}
-              <div className="flex mt-5 gap-4">
+              <div className="flex gap-4">
                 <a
                   href="https://www.facebook.com/profile.php?id=61574682066111"
                   target="_blank"
