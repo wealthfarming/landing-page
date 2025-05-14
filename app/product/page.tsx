ce0502ca2812c8faca4be0431b12534309147016
PaTuanas <phanhtuan05@gmail.com>
2025-05-14 12:06:31 +0700

    add inView props

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -7,15 +7,20 @@ import { AnimatedText } from "@/components/animation/introduction/Animations"
 import Image from "next/image";
 import productBase from "../../public/images/img/product_base.jpg"
 import { useTranslation } from "react-i18next";
+import { useInView } from "react-intersection-observer";
 
 export default function ProductPage() {
     const { t } = useTranslation();
     const { isDesktop, isTablet } = useInterface();
+    const { ref, inView } = useInView({
+        triggerOnce: true,
+        threshold: 0.1,
+    });
     return (
         <div className={`${isDesktop ? 'pb-[605px]' : isTablet ? '' : ''}`}>
             <HeaderDesktopFull changeAt={300} />
             {isDesktop &&
-                <div className="w-full h-[260px] relative" style={{zIndex: 100}}>
+                <div className="w-full h-[260px] relative" style={{ zIndex: 100 }}>
                     <div className="absolute inset-0 bg-black/50 z-10"></div>
                     <Image src={productBase} alt="Product Banner" width={735} height={260} className="w-full h-[260px] object-cover" />
                 </div>
@@ -23,12 +28,13 @@ export default function ProductPage() {
             <div className={`w-full flex-grow flex flex-col items-center justify-start bg-background ${isDesktop ? ' mt-0 pb-[40px]' : ' mt-18 '} relative z-30`}>
 
                 <h1 className="lg:text-7xl md:text-5xl text-4xl font-normal mb-4 w-full md:w-9/12 max-w-[1200px] mt-[40px] px-[20px] flex flex-col gap-6 text-center justify-center items-center py-2 ">
-                    <div className="max-w-[904px] w-full bigTitle">
+                    <div className="max-w-[904px] w-full bigTitle" ref={ref}>
                         <AnimatedText
                             customClass={[`${isDesktop ? 'text-[49px] leading-[58.8px] flex flex-wrap' : isTablet ? 'text-[44px] leading-[52.8px] flex flex-wrap' : 'text-[34px] leading-[40.8px] flex flex-wrap'}`]}
                             text={[t('page_product_title')]}
                             delayBetween={0.05}
                             duration={0.3}
+                            inView={inView}
                         />
                     </div>
                     <p className=" w-full max-w-[721px] text-center body !font-geist-display">{t('page_product_description')}</p>
