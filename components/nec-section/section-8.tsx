0d2d7362f56dcae6d8887e15654623c84b7f69d5
lvquan2003 <levanquan051003@gmail.com>
2025-05-15 18:15:20 +0700

    nec-section-8-9, footer

--- components/nec-section/section-8.tsx
+++ components/nec-section/section-8.tsx
@@ -1,11 +1,30 @@
 'use client';
+
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import Image from "next/image";
+import { useInterface } from '@/components/context/interface-context';
+import section9 from "../../public/images/home-section-7/section-7.png"
 
 const Section8: React.FC = () => {
+    const { isDesktop, isTablet, isMobile } = useInterface();
+    const { t } = useTranslation();
+
     return (
-        <section className="nec-section nec-section-8">
-            <h2>Nec Section 8</h2>
-            {/* ...additional content... */}
+        <section className={`w-full ${isDesktop ? 'h-[264px]' : isTablet ? 'h-[214px]' : 'h-[176px]'} relative`}>
+            <Image
+                src={section9}
+                alt=""
+                className={`w-full ${isDesktop ? 'h-[264px]' : isTablet ? 'h-[214px]' : 'h-[176px]'} object-cover`}
+            />
+
+            <div className="absolute inset-0 bg-black/50 z-10"></div>
+
+            <div className={`absolute z-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[1200px] ${!isMobile ? 'px-[40px]' : 'px-[20px]'}`}>
+                <p className={`${isMobile ? 'h2' : 'h1'} !text-white text-center`}>
+                    {t("nec_section_8_title_1")}
+                </p>
+            </div>
         </section>
     );
 };
