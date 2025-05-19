c507f28769ee66d68b3cba921f82536686f76938
buichibaobeqholdings <buichibao.beqholdings@gmail.com>
2025-05-19 15:12:01 +0700

    update

--- components/nec-section/section-5.tsx
+++ components/nec-section/section-5.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Section5: React.FC = () => {
     return (
-        <section className="nec-section nec-section-5">
+        <section className="nec-section nec-section-5 h-[900px]">
             <h2>Nec Section 5</h2>
             {/* ...additional content... */}
         </section>
