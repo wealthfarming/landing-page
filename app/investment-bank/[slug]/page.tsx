e3cb1f3354148352c303dcd849e06baba776a051
HDNghia <huynhdangnghia68@gmail.com>
2025-05-19 16:34:15 +0700

    update file yml

--- app/investment-bank/[slug]/page.tsx
+++ app/investment-bank/[slug]/page.tsx
@@ -3,13 +3,13 @@ import InvestmentBankClient from '@/components/posts/investment-bank-client'
 import { API_URL } from '@/lib/config';
 import { Apiget }  from '@/lib/api/get';
 
-// export async function generateStaticParams(): Promise<{ slug: string }[]> {
-//   const allPosts: any[] = await Apiget(
-//     `${API_URL}/api/posts`,
-//     { sort: 'createdAt', limit: 1000, locale: 'en' }
-//   );
-//   return allPosts.map(post => ({ slug: post.route as string }));
-// }
+export async function generateStaticParams(): Promise<{ slug: string }[]> {
+  const allPosts: any[] = await Apiget(
+    `${API_URL}/api/posts`,
+    { sort: 'createdAt', limit: 1000, locale: 'en' }
+  );
+  return allPosts.map(post => ({ slug: post.route as string }));
+}
 
 // no type annotation here—Next will supply { params, searchParams }
 export default function Page({ params }: any) {
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
-  output: 'standalone',
+  output: 'export',
   basePath: '',
   assetPrefix: '',
   trailingSlash: true,
