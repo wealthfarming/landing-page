ac2a08ae9acce727396db28924708d389e5a7bfa
PaTuanas <phanhtuan05@gmail.com>
2025-06-02 12:03:16 +0700

    Update slug en

--- app/investment-bank/page.tsx
+++ app/investment-bank/page.tsx
@@ -4,18 +4,16 @@ import HeaderDesktopFull from "@/components/header/header-desktop";
 import Image from "next/image";
 import { useInterface } from '@/components/context/interface-context';
 import { useTranslation } from "react-i18next";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { AnimatedText } from "@/components/animation/introduction/Animations";
-import { Apiget } from "@/lib/api/get"
+import { Apiget } from "@/lib/api/get";
 import { API_URL } from "@/lib/config";
-import { useEffect } from 'react';
-
-import productBase from "../../public/images/img/product_base.jpg"
+import productBase from "../../public/images/img/product_base.jpg";
 import { useInView } from "react-intersection-observer";
+
 export default function InvestmentBank() {
   const { isDesktop, isMobile, isTablet } = useInterface();
   const { t, i18n } = useTranslation();
-
   const [selected, setSelected] = useState('');
   const [tabs, setTabs] = useState<any[]>([]);
   const [contents, setContents] = useState<any[]>([]);
@@ -24,33 +22,23 @@ export default function InvestmentBank() {
     threshold: 0.1,
   });
 
-
   useEffect(() => {
     const getPostCategories = async () => {
-      const data_en = await Apiget(
-        API_URL + '/api/posts-categories',
-        {
-          sort: 'createdAt',
-          limit: 0,
-          locale: 'en',
-        }
-      );
-      const data_vi = await Apiget(
-        API_URL + '/api/posts-categories',
-        {
-          sort: 'createdAt',
-          limit: 0,
-          locale: 'vi',
-        }
-      );
-      const data_fr = await Apiget(
-        API_URL + '/api/posts-categories',
-        {
-          sort: 'createdAt',
-          limit: 0,
-          locale: 'fr',
-        }
-      );
+      const data_en = await Apiget(API_URL + '/api/posts-categories', {
+        sort: 'createdAt',
+        limit: 0,
+        locale: 'en',
+      });
+      const data_vi = await Apiget(API_URL + '/api/posts-categories', {
+        sort: 'createdAt',
+        limit: 0,
+        locale: 'vi',
+      });
+      const data_fr = await Apiget(API_URL + '/api/posts-categories', {
+        sort: 'createdAt',
+        limit: 0,
+        locale: 'fr',
+      });
       const postscategories = data_en.map((itemEn: any) => {
         const itemVi = data_vi.find((itemVi: any) => itemVi.id === itemEn.id);
         const itemFr = data_fr.find((itemFr: any) => itemFr.id === itemEn.id);
@@ -69,34 +57,24 @@ export default function InvestmentBank() {
 
   useEffect(() => {
     const getPosts = async () => {
-
-      const data_en = await Apiget(
-        API_URL + '/api/posts',
-        {
-          'where[category][equals]': selected,
-          sort: 'createdAt',
-          limit: 0,
-          locale: 'en',
-        }
-      );
-      const data_vi = await Apiget(
-        API_URL + '/api/posts',
-        {
-          'where[category][equals]': selected,
-          sort: 'createdAt',
-          limit: 0,
-          locale: 'vi',
-        }
-      );
-      const data_fr = await Apiget(
-        API_URL + '/api/posts',
-        {
-          'where[category][equals]': selected,
-          sort: 'createdAt',
-          limit: 0,
-          locale: 'fr',
-        }
-      );
+      const data_en = await Apiget(API_URL + '/api/posts', {
+        'where[category][equals]': selected,
+        sort: 'createdAt',
+        limit: 0,
+        locale: 'en',
+      });
+      const data_vi = await Apiget(API_URL + '/api/posts', {
+        'where[category][equals]': selected,
+        sort: 'createdAt',
+        limit: 0,
+        locale: 'vi',
+      });
+      const data_fr = await Apiget(API_URL + '/api/posts', {
+        'where[category][equals]': selected,
+        sort: 'createdAt',
+        limit: 0,
+        locale: 'fr',
+      });
       const posts = data_en.map((itemEn: any) => {
         const itemVi = data_vi.find((itemVi: any) => itemVi.id === itemEn.id);
         const itemFr = data_fr.find((itemFr: any) => itemFr.id === itemEn.id);
@@ -108,7 +86,7 @@ export default function InvestmentBank() {
           title_vi: itemVi.title,
           title_en: itemEn.title,
           title_fr: itemFr.title,
-          date: itemEn.createdAt
+          date: itemEn.createdAt,
         };
       });
       setContents(posts);
@@ -143,7 +121,7 @@ export default function InvestmentBank() {
           <Image src="/images/img/section_4_2.png" alt="Background Image" layout="fill" objectFit="cover" />
         </div>
         <div className={`flex ${!isMobile ? 'flex-row' : 'flex-col'} gap-[40px] w-[1200px] z-10`}>
-          <div className="gap-[40px] flex-col items-center lg:w-[336px] w-full  min-w-[336px] ">
+          <div className="gap-[40px] flex-col items-center lg:w-[336px] w-full min-w-[336px] ">
             {tabs.map((tab) => (
               <div
                 key={tab.id}
@@ -168,42 +146,39 @@ export default function InvestmentBank() {
               <div key={content.id}>
                 <div className="h-[97px] py-[20px] border-b border-[var(--primary-other)]">
                   <div className="flex flex-col gap-[10px] justify-center">
-                    <a href={`/investment-bank/${content[`slug_${i18n.language}`] || content[`slug_en`] || content[`slug_vi`] || content[`slug_fr`] || '#'
-                      }`}>
+                    <a href={`/investment-bank/${content.slug_en}`}>
                       <p className="text-[15px] hover:text-[#f1c204] cursor-pointer transition-colors">
                         {content[`title_${i18n.language}`] || content[`title_en`] || content[`title_fr`] || 'No title'}
                       </p>
                     </a>
                     <p className="text-light text-[13px]">
-                      {
-                        i18n.language === 'vi'
-                          ? new Intl.DateTimeFormat('vi-VN', {
+                      {i18n.language === 'vi'
+                        ? new Intl.DateTimeFormat('vi-VN', {
+                          year: 'numeric',
+                          month: 'long',
+                          day: 'numeric',
+                        }).format(new Date(content.date))
+                        : i18n.language === 'fr'
+                          ? new Intl.DateTimeFormat('fr-FR', {
                             year: 'numeric',
                             month: 'long',
                             day: 'numeric',
                           }).format(new Date(content.date))
-                          : i18n.language === 'fr'
-                            ? new Intl.DateTimeFormat('fr-FR', {
-                              year: 'numeric',
-                              month: 'long',
-                              day: 'numeric',
-                            }).format(new Date(content.date))
-                            : new Intl.DateTimeFormat('en-US', {
-                              year: 'numeric',
-                              month: 'long',
-                              day: 'numeric',
-                            }).format(new Date(content.date))
+                          : new Intl.DateTimeFormat('en-US', {
+                            year: 'numeric',
+                            month: 'long',
+                            day: 'numeric',
+                          }).format(new Date(content.date))
                       }
                     </p>
                   </div>
                 </div>
               </div>
             ))}
-
           </div>
         </div>
       </div>
       <FooterFull active={'investment-bank'} />
-    </div >
+    </div>
   );
 }
